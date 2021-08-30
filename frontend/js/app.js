d7ff315e8f5c3ab92a3ae0a44bc0b81eeec341c6
wucéline <wuwuceline@gmail.com>
2021-08-30 22:20:32 +0200

    afficher les reviews ok

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,6 @@
 let app = {
+    apiRootUrl: "http://localhost:8080",
+    
     init: function() {
         console.log('app.init()');
 
@@ -12,6 +14,8 @@ let app = {
         // On récupère l'élément <select> des jeux vidéo
         
         // On ajoute l'écouteur pour l'event "change", et on l'attache à la méthode app.handleVideogameSelected
+        let videoGameSelectElement = document.querySelector("#videogameId");
+        videoGameSelectElement.addEventListener('change', app.handleVideogameSelected)
 
         // On récupère le bouton pour ajouter un jeu vidéo
         let addVideogameButtonElement = document.getElementById('btnAddVideogame');
@@ -22,13 +26,77 @@ let app = {
     },
     handleVideogameSelected: function(evt) {
         // Récupérer la valeur du <select> (id du videogame)
+        videoGameId = evt.currentTarget.value;
+        console.log(videoGameId);
 
         // Vider le contenu de div#review
+        // const reviewContainer = document.querySelector('#review');
+        // reviewContainer.removeChild();
+
 
         // charger les données pour ce videogame
-            // Dupliquer la template #reviewTemplate et personnaliser son contenu avec les données
+        let config = {
+            method : 'GET',
+            mode: 'cors',
+            cache: 'no-cache'
+        };
 
-            // Ajouter dans le DOM
+        fetch(app.apiRootUrl + "/videogames/"+videoGameId+"/reviews/", config)
+        .then(function(response){
+          return response.json();
+        })
+        .then(function(reviews){
+                console.log(reviews);
+
+
+                for(const review of reviews){
+                    // Dupliquer la template #reviewTemplate et personnaliser son contenu avec les données
+                    const templateElement = document.querySelector('#reviewTemplate');
+                    const templateClonedElement = templateElement.content.cloneNode(true);
+
+                    const reviewElement = templateClonedElement.querySelector('.reviewContainer');
+                    const reviewContainer = document.querySelector('#review');
+
+                    // Supprimer les reviews
+                    // reviewContainer.removeChild(reviewContainer);
+                    
+                    // AUTHOR
+                    reviewAuthorElement = reviewElement.querySelector('.reviewAuthor');
+                    reviewAuthorElement.textContent = review.author;
+                    
+                    // TEXT 
+                    reviewTextElement = reviewElement.querySelector('.reviewText');
+                    reviewTextElement.textContent = review.text;
+                    
+                    // TITLE 
+                    reviewTitleElement = reviewElement.querySelector('.reviewTitle');
+                    reviewTitleElement.textContent = review.title;
+                    
+                    // PUBLICATION
+                    reviewPublicationElement = reviewElement.querySelector('.reviewPublication');
+                    reviewPublicationElement.textContent = review.publication_date;
+                    
+                    // REVIEW NOTES
+                    reviewDisplayNoteElement = reviewElement.querySelector('.reviewDisplay');
+                    reviewDisplayNoteElement.textContent = review.display_note;
+
+                    reviewGameplayNoteElement = reviewElement.querySelector('.reviewGameplay');
+                    reviewGameplayNoteElement.textContent = review.gameplay_note;
+
+                    reviewScenarioNoteElement = reviewElement.querySelector('.reviewScenario');
+                    reviewScenarioNoteElement.textContent = review.scenario_note;
+
+                    reviewLifetimeNoteElement = reviewElement.querySelector('.reviewLifetime');
+                    reviewLifetimeNoteElement.textContent = review.lifetime_note;
+
+                    // Ajouter dans le DOM
+                    reviewContainer.prepend(reviewElement);
+                };
+
+            
+        });
+
+        
     },
     handleClickToAddVideogame: function(evt) {
         // https://getbootstrap.com/docs/4.4/components/modal/#modalshow
